68c0f1c42a50208ef17a68646362ad5be7e9bdd3
awa <kenshin561183@gmail.com>
2020-10-05 23:19:02 +0900

    refactored and change directories

--- config_db/database.py
+++ config_db/database.py
@@ -5,7 +5,8 @@ from peewee import *
 db = MySQLDatabase(
   'recruit',
   **{'user': 'root',
-     'host': '172.29.0.2',
+     'host': 'con_db',
      'password': 'root'}
 )
 db.connect()
+db.close()
