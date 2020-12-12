51172c3dd91f2cc51de0c6496270173f291e3b8d
awa <kenshin561183@gmail.com>
2020-12-12 19:06:34 +0900

    refactored base_scraping

--- base_scraping.py
+++ base_scraping.py
@@ -16,8 +16,10 @@ class BaseScraping():
         logging.config.fileConfig('config_python/logging.conf')
         self.logger = logging.getLogger(self.__class__.__name__)
         self.table = None
-        self.BASE_PAGE = 'https://doda.jp/DodaFront/View/JobSearchList.action?ss=1&pic=1&ds=0&so=50&tp=1'
-        self.OTHER_PAGE = 'https://doda.jp/DodaFront/View/JobSearchList.action?pic=1&ds=0&so=50&tp=1&page='
+        self.current_page_url = None
+        self.match_tel = re.compile(r'[\(]{0,1}[0-9]{2,4}[\)\-\(‐]{0,1}[0-9]{2,4}[\)\-－]{0,1}[0-9]{3,4}')
+        self.match_post = re.compile(r'[0-9]{3}-[0-9]{4}')
+        self.present_uri = None
 
     def drop_table(self, table_name: str):
         """ 既存のテーブルを削除する """
@@ -25,6 +27,7 @@ class BaseScraping():
             if table_name in db.get_tables():
                 db.drop_tables(self.table.__class__)
             db.create_tables([self.table.__class__])
+            self.logger.info("recreated %s_table." % table_name)
         except Exception as e:
             self.logger.error("failed to drop %s table" % table_name)
             self.logger.exception(e)
@@ -35,52 +38,61 @@ class BaseScraping():
         parser = BeautifulSoup(html.text, "html.parser")
         return parser
 
+    def how_many_job_offers_exists(self, parser: BeautifulSoup) -> int:
+        """ 求人数を取得する """
+        try:
+            max_job_offers = int(parser.find_all("span", class_="js-resultCount")[-1].string)
+            self.logger.info("Max Number of Job-offers is %s" % max_job_offers)
+            return max_job_offers
+        except Exception as e:
+            self.logger.error("failed to get number of max job offers")
+            self.logger.exception(e)
+
     def how_many_pages_exists(self, parser: BeautifulSoup) -> int:
         """ ページネーションから最大ページ数を取得する """
         max_page_number = int(parser.find_all("a", class_="pagenation")[-1].string)
         self.logger.info("Max Page is %s" % max_page_number)
         return max_page_number
     
-    def get_per_page_urls(self, max_page_number: int) -> list:
+    def get_companies_links_on_the_page(self, page_parser: BeautifulSoup, link_condition: str) -> list:
+        """ 各企業の求人詳細ページリンクを取得する """
+        try:
+            links = page_parser.find_all("a")
+            companies_links_list = []
+            for link in links:
+                if "class" not in link.attrs or link_condition not in link['class']:
+                    continue
+                company_link = link.get("href")
+                if company_link not in companies_links_list:
+                    companies_links_list.append(company_link)
+            return companies_links_list
+        except Exception as e:
+            self.logger.error("failed to get companies_links on the page:%s" % self.current_page_url)
+            self.logger.exception(e)
+
+    def get_next_page_url(self, pagenations_parser: BeautifulSoup, pagenation_terms: dict, current_terms: dict) -> str:
         """ ページ数分の各URLを取得する """
-        per_page_urls = [self.BASE_PAGE] + [self.OTHER_PAGE + str(i) for i in range(2, max_page_number + 1)]
-        self.logger.info("got each pages URL.")
-        return per_page_urls
+        try:
+            pagenations = pagenations_parser.find_all(pagenation_terms['tag'], class_=pagenation_terms['class'])
+            current_page_num = int(pagenations_parser.find(current_terms['tag'], class_=current_terms['class']).text)
+            next_page_num = current_page_num + 1
+            for pagenation in pagenations:
+                if int(pagenation.text) == next_page_num:
+                    next_page_url = pagenation.get("href")
+                    return next_page_url
+        except Exception as e:
+            self.logger.error("couldn't get next page url.")
+            self.logger.exception(e)
 
-    def get_next_page_url(self, pegenations_parser: BeautifulSoup) -> str:
-        """ ページ数分の各URLを取得する """
-        pagenations = pegenations_parser.find_all("a", class_="pagenation")
-        current_page_num = int(pegenations_parser.find("span", class_="current").text)
-        next_page_num = current_page_num + 1
-        for pagenation in pagenations:
-            if int(pagenation.text) == next_page_num:
-                next_page_url = pagenation.get("href")
-                return next_page_url
-        self.logger.error("couldn't get next page url.")
-
-    def search_recruit_info(self, per_page_urls: list) -> list:
-        company_urls = []
-        for page_url in per_page_urls:
-            html = requests.get(page_url)
-            parser = BeautifulSoup(html.text, "html.parser")
-            key_tags = parser.find_all("a")
-            company_url = self._company_info(key_tags)
-            company_urls += company_url
-        return company_urls
-
-    @staticmethod
-    def _company_info(key_tags) -> list:
-        set_company_link = []
-        for key_tag in key_tags:
-            if "class" not in key_tag.attrs or key_tag['class'] != ["_JobListToDetail"]:
-                continue
-            company_link = key_tag.get("href")
-            if company_link not in set_company_link:
-                set_company_link.append(company_link)
-        return set_company_link
-
-    def get_company_name(self, parser: BeautifulSoup) -> str:
-        return parser.find("p", "job_title").string
+    def get_company_name(self, parser: BeautifulSoup, company_terms: dict) -> str:
+        """ 会社名を取得する """
+        try:
+            company_name_raw_data = parser.find(company_terms['tag'], company_terms['class']).string
+            company_name = self.replace_text(company_name_raw_data, company=True)
+            return company_name
+        except Exception as e:
+            self.logger.error("failed to get company_name.")
+            self.logger.exception(e)
 
     def get_address(self, parser: BeautifulSoup) -> tuple:
         pass
@@ -89,12 +101,17 @@ class BaseScraping():
         pass
 
     def get_commany_hp(self, parser: BeautifulSoup, match_text=None) -> tuple:
-        company_hp = ""
-        hp_tag = parser.find(text=match_text)
-        if hp_tag is not None:
-            company_hp = hp_tag.parent.parent.a.text
-            company_hp = company_hp.replace(" ", "").replace("\r\n", "").replace("\n", "")
-        return company_hp
+        """ 企業ホームページを取得する """
+        try:
+            company_hp = ""
+            hp_tag = parser.find(text=match_text)
+            if hp_tag is not None:
+                company_hp_raw_data = hp_tag.parent.parent.a.text
+                company_hp = self.replace_text(company_hp_raw_data)
+            return company_hp
+        except Exception as e:
+            self.logger.error("failed to get the commany's website URI.")
+            self.logger.exception(e)
 
     def replace_text(self, raw_data: str, company=False, working_office=False) -> str:
         try:
